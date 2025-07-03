36dcb43bf1603252a4123465c3427311d0eadda6
logidhasan <logidhasan25@gmail.com>
2025-07-03 19:07:12 +0530

    idcolumn defined

--- src/pipeline/data/tabular.py
+++ src/pipeline/data/tabular.py
@@ -1,5 +1,35 @@
-from ..base import Pipeline
+import os
 
-class Pipeline:
-    def __init__(self):
+try:
+    import pandas as pd
+
+    PANDAS = True
+except ImportError:
+    PANDAS = False
+
+class Tabular:
+    def __init__(self, idcolumn=None, textcolumns=None, content=False):
+        if not PANDAS:
+            raise ImportError("install pandas")
+        
+        self.idcolumn = idcolumn
+        self.textcolumns = textcolumns
+        self.content = content
+
+    def process(self, df):
+        rows = []
+        columns = self.textcolumns
+        
+        if not columns:
+            columns = list(df.columns)
+            if self.idcolumn:
+                columns.remove(self.idcolumn)
+            
+        for index, row in df.iterrows():
+            uid = row[self.idcolumn] if self.idcolumn else index
+            uid = uid if uid is not None else index
+            text = self.concat(row, columns)
+
+    
+    def concat(self, row, columns):
         pass
